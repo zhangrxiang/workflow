442ff65a56f7392a103e1485ece1dc68493038e1
zing <599490911@qq.com>
2019-02-27 16:40:06 +0800

    [Update]首页代码格式化

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Entry,App\Proc,App\Flowlink,App\ProcessVar,App\Flow,App\EntryData,App\Emp;
-
-use Auth,DB;
+use App\Entry,
+    App\Proc,
+    App\Flow;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -19,25 +18,52 @@ class HomeController extends Controller
     public function index()
     {
         //我的申请
-        $entries=Entry::with(["emp","procs"=>function($query){
-            $query->orderBy("id",'DESC')->take(1);
-        },"process"])->where(['emp_id'=>Auth::id(),'pid'=>0])->orderBy('id','DESC')->get();
+        $entries = Entry::with([
+            "emp",
+            "procs" => function ($query) {
+                $query->orderBy("id", 'DESC')->take(1);
+            },
+            "process"
+        ])
+            ->where([
+                'emp_id' => Auth::id(),
+                'pid' => 0
+            ])
+            ->orderBy('id', 'DESC')
+            ->get();
 
-        // dd($entries);
-        
         //我的待办
-        $procs=Proc::with(["emp","entry"=>function($query){
-            $query->with("emp");
-        }])->where(['emp_id'=>Auth::id(),'status'=>0])->orderBy("is_read","ASC")->orderBy("status","ASC")->orderBy("id","DESC")->get();
+        $procs = Proc::with([
+            "emp",
+            "entry" => function ($query) {
+                $query->with("emp");
+            }])
+            ->where([
+                'emp_id' => Auth::id(),
+                'status' => 0
+            ])
+            ->orderBy("is_read", "ASC")
+            ->orderBy("status", "ASC")
+            ->orderBy("id", "DESC")
+            ->get();
 
         //工作流 分组TODO
-        $flows=Flow::where(['is_publish'=>1,'is_show'=>1])->orderBy('id','ASC')->get();
-        $handle_procs=Proc::with(["emp","entry"=>function($query){
+        $flows = Flow::where([
+            'is_publish' => 1,
+            'is_show' => 1
+        ])
+            ->orderBy('id', 'ASC')
+            ->get();
+
+        $handle_procs = Proc::with(["emp", "entry" => function ($query) {
             $query->with("emp");
-        }])->where(['emp_id'=>Auth::id()])->where('status','!=',0)->orderBy('entry_id','DESC')->orderBy("id","ASC")->get()->groupBy('entry_id');
-
-        // dd($handle_procs);
-
-        return view('home')->with(compact("entries","procs","flows","handle_procs"));
+        }])
+            ->where(['emp_id' => Auth::id()])
+            ->where('status', '!=', 0)
+            ->orderBy('entry_id', 'DESC')
+            ->orderBy("id", "ASC")
+            ->groupBy('entry_id')
+            ->get();
+        return view('home')->with(compact("entries", "procs", "flows", "handle_procs"));
     }
 }
